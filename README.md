7da97ad05410c20de1aabfef9c92824065eed7c3
rzlamrr <rzlamrr.dvst@protonmail.com>
2021-07-24 20:20:51 +0000

    Add extras for p404_lavender-user 11 RKQ1.210614.002 eng.raiyan.20210703.024911 release-keys
    Signed-off-by: rzlamrr <rzlamrr.dvst@protonmail.com>

---
+++ README.md
@@ -0,0 +1,18 @@
+## p404_lavender-user 11 RKQ1.210614.002 eng.raiyan.20210703.024911 release-keys
+- Manufacturer: xiaomi
+- Platform: sdm660
+- Codename: lavender
+- Brand: Xiaomi
+- Flavor: p404_lavender-user
+- Release Version: 11
+- Id: RKQ1.210614.002
+- Incremental: eng.raiyan.20210703.024911
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: google/redfin/redfin:11/RQ3A.210605.003/7349499:user/release-keys
+- OTA version: 
+- Branch: p404_lavender-user-11-RKQ1.210614.002-eng.raiyan.20210703.024911-release-keys
+- Repo: xiaomi_lavender_dump
---
+++ join_split_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/product/priv-app/RecorderPrebuilt/RecorderPrebuilt.apk.* 2>/dev/null >> system/system/product/priv-app/RecorderPrebuilt/RecorderPrebuilt.apk
+rm -f system/system/product/priv-app/RecorderPrebuilt/RecorderPrebuilt.apk.* 2>/dev/null
+cat system/system/product/app/Messages/Messages.apk.* 2>/dev/null >> system/system/product/app/Messages/Messages.apk
+rm -f system/system/product/app/Messages/Messages.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f system/system/product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat system/system/product/app/Photos/Photos.apk.* 2>/dev/null >> system/system/product/app/Photos/Photos.apk
+rm -f system/system/product/app/Photos/Photos.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
